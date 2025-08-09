17fe6078e1ac8eae28888bea072d0201e9168767
nikhillad123 <nikhil.lad24india@gmail.com>
2025-08-09 16:43:07 +0530

    Creating Hero, SearchBar and Home page

--- educrave-project/src/pages/student/Home.jsx
+++ educrave-project/src/pages/student/Home.jsx
@@ -1,7 +1,9 @@
+import Hero from "../../components/student/Hero";
+
 const Home = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className="flex flex-col items-center space-y-7 text-center">
+            <Hero />
         </div>
     );
 }
