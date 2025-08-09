822cd5bc897dfcd439cd823a96e4b730b92bff4f
nikhillad123 <nikhil.lad24india@gmail.com>
2025-08-09 21:14:12 +0530

    Creating calltoaction, footer and testimonialSection files and finally completed the Home page

--- educrave-project/src/pages/student/Home.jsx
+++ educrave-project/src/pages/student/Home.jsx
@@ -1,6 +1,9 @@
+import CallToAction from "../../components/student/CallToAction";
 import Companies from "../../components/student/Companies";
 import CoursesSection from "../../components/student/CoursesSection";
+import Footer from "../../components/student/Footer";
 import Hero from "../../components/student/Hero";
+import TestimonialSection from "../../components/student/TestimonialSection";
 
 const Home = () => {
     return (
@@ -8,6 +11,9 @@ const Home = () => {
             <Hero />
             <Companies />
             <CoursesSection />
+            <TestimonialSection />
+            <CallToAction />
+            <Footer />
         </div>
     );
 }
