ab28bda1745f146eb38ac7acb78189243f8228a9
nikhillad123 <nikhil.lad24india@gmail.com>
2025-08-09 15:43:45 +0530

    Completing Navbar code in both laptop and mobile.

--- educrave-project/src/components/student/Navbar.jsx
+++ educrave-project/src/components/student/Navbar.jsx
@@ -1,41 +1,65 @@
 import { assets } from '../../assets/assets';
 import { Link } from 'react-router-dom';
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 
 const Navbar = () => {
     const isCourseListPage = location.pathname.includes('/course-list');
 
+    const { openSignIn } = useClerk();
+    const { user } = useUser();
+
     return (
         <div
-            className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${
-                isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'
-            }`}
+            className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'
+                }`}
         >
+            {/* Logo */}
             <img
                 src={assets.logo}
                 alt="Logo"
                 className="w-28 lg:w-32 cursor-pointer"
             />
 
+            {/* Desktop Menu */}
             <div className="hidden md:flex items-center gap-5 text-gray-500">
-                <div>
-                    <button>Become Educator</button>
-                    {' | '}
-                    <Link to="/my-enrollments">My Enrollments</Link>
-                </div>
-                <button className="bg-blue-600 text-white px-5 py-2 rounded-full">
-                    Create Account
-                </button>
+                {user && (
+                    <>
+                        <button>Become Educator</button>
+                        <span className="mx-2">|</span>
+                        <Link to="/my-enrollments">My Enrollments</Link>
+                    </>
+                )}
+
+                {user ? (
+                    <UserButton />
+                ) : (
+                    <button
+                        onClick={() => openSignIn()}
+                        className="bg-blue-600 text-white px-5 py-2 rounded-full"
+                    >
+                        Create Account
+                    </button>
+                )}
             </div>
 
+            {/* Mobile Menu */}
             <div className="md:hidden flex items-center gap-2 text-gray-500">
-                <div>
-                    <button>Become Educator</button>
-                    <Link to="/my-enrollments">My Enrollments</Link>
+                <div className="flex items-center gap-1 sm:gap-2 max-sm:text-xs">
+                    {user && (
+                        <>
+                            <button>Become Educator</button>
+                            <span className="mx-2">|</span>
+                            <Link to="/my-enrollments">My Enrollments</Link>
+                        </>
+                    )}
                 </div>
-                <button><img src={assets.user_icon} alt="user icon" /></button>
+                {
+                    user ? <UserButton />
+                    : <button onClick={() => openSignIn()}><img src={assets.user_icon} alt="user icon" /></button>
+                }
             </div>
         </div>
     );
-}
+};
 
 export default Navbar;
