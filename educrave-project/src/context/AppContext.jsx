951a57858f9bc0b35f75ebd8d4e4fa44427a2362
nikhillad123 <nikhil.lad24india@gmail.com>
2025-08-10 14:28:11 +0530

    Creating CourseDetails page file

--- educrave-project/src/context/AppContext.jsx
+++ educrave-project/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
-import { createContext, useState, useEffect } from 'react';
-import { dummyCourses } from '../assets/assets';
-import { useNavigate } from 'react-router-dom';
+import { createContext, useState, useEffect } from 'react'
+import { dummyCourses } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+import humanizeDuration from 'humanize-duration'
 
 const AppContext = createContext();
 
@@ -30,13 +31,38 @@ export const AppContextProvider = (props) => {
         return totalRating / course.courseRatings.length
     }
 
+    // function to calculate course chapter time
+    const calculateChapterTime = (chapter) => {
+        let time = 0
+        chapter.chapterContent.map((lecture) => time += lecture.lectureDuration)
+        return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] })
+    }
+
+    // function to calculate course duration
+    const calculateCourseDuration = (course) => {
+        let time = 0
+        course.courseContent.map((chapter) => chapter.chapterContent.map((lecture) => time += lecture.lectureDuration))
+        return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] })
+    }
+
+    // Function to calculate No of Lectures in the course
+    const calculateNoOfLectures = (course) => {
+        let totalLectures = 0;
+        course.courseContent.forEach(chapter => {
+            if (Array.isArray(chapter.chapterContent)) {
+                totalLectures += chapter.chapterContent.length
+            }
+        });
+        return totalLectures;
+    }
+
     useEffect(() => {
         fetchAllCourses();
     }, []);
 
     const value = {
         // Add shared state or functions here
-        currency, allCourses, navigate, calculateRating, isEducator
+        currency, allCourses, navigate, calculateRating, isEducator, calculateChapterTime, calculateCourseDuration, calculateNoOfLectures
     }
 
     return (
