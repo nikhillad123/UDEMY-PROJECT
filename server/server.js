37406eb876e0a7d30ba7d2c485f9875e805a97e1
nikhillad123 <nikhil.lad24india@gmail.com>
2025-08-12 21:57:31 +0530

    Setting up the backend with nodejs and expressjs and using mongodb atlas and mongoose as database

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/mongodb.js'
+
+// initialize Express
+const app = express();
+
+// Connect to database
+await connectDB();
+
+// Middlewares
+app.use(cors());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send("API Working...")
+});
+
+// Port
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
