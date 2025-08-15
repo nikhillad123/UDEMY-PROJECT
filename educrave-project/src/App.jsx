46838982302da75a87a016d1728d3082009a80b2
nikhillad123 <nikhil.lad24india@gmail.com>
2025-08-15 22:38:18 +0530

    Connecting both frontend and backend. Also making the data come to the frontend through database.

--- educrave-project/src/App.jsx
+++ educrave-project/src/App.jsx
@@ -12,6 +12,7 @@ import MyCourses from './pages/educator/MyCourses'
 import StudentsEnrolled from './pages/educator/StudentsEnrolled'
 import Navbar from './components/student/Navbar'
 import "quill/dist/quill.snow.css"
+import { ToastContainer } from 'react-toastify'
 
 import './index.css'
 import './App.css'
@@ -21,6 +22,7 @@ function App() {
 
   return (
     <div className="text-default min-h-screen bg-white">
+      <ToastContainer />
       {!isEducatorRoute && <Navbar />} 
       <Routes>
         <Route path="/" element={<Home />} />
--- educrave-project/src/context/AppContext.jsx
+++ educrave-project/src/context/AppContext.jsx
@@ -1,12 +1,16 @@
 import { createContext, useState, useEffect } from 'react'
-import { dummyCourses } from '../assets/assets'
+// import { dummyCourses } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 import humanizeDuration from 'humanize-duration'
 import { useAuth, useUser } from '@clerk/clerk-react'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
+
+    const backendUrl = import.meta.VITE_BACKEND_URL;
 
     const currency = import.meta.env.VITE_CURRENCY;
     const navigate = useNavigate();
@@ -15,13 +19,51 @@ export const AppContextProvider = (props) => {
     const { user } = useUser();
 
     const [allCourses, setAllCourses] = useState([]);
-    const [isEducator, setIsEducator] = useState(true);
+    const [isEducator, setIsEducator] = useState(false);
     const [enrolledCourses, setEnrolledCourses] = useState([]);
+    const [userData, setUserData] = useState(null);
 
     // Fetch all Courses
     const fetchAllCourses = async () => {
-        setAllCourses(dummyCourses);
+        // setAllCourses(dummyCourses);
+
+        try {
+            const { data } = await axios.get(backendUrl + '/api/course/all');
+
+            if (data.success) {
+                setAllCourses(data.courses);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
     }
+
+    // Fetch userData
+    const fetchUserData = async () => {
+        if (user.publicMetadata.role === 'educator') {
+            setIsEducator(true);
+        }
+
+        try {
+            const token = await getToken();
+
+            const { data } = await axios.get(backendUrl + '/api/user/data', {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+
+            if (data.success) {
+                setUserData(data.user);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
 
     const calculateRating = (course) => {
         if (course.courseRatings.length === 0) {
@@ -33,7 +75,7 @@ export const AppContextProvider = (props) => {
             totalRating += rating.rating
         })
 
-        return totalRating / course.courseRatings.length
+        return Math.floor(totalRating / course.courseRatings.length);
     }
 
     // function to calculate course chapter time
@@ -63,7 +105,20 @@ export const AppContextProvider = (props) => {
 
     // Fetch user enrolled courses
     const fetchUserEnrolledCourses = async () => {
-        setEnrolledCourses(dummyCourses);
+        // setEnrolledCourses(dummyCourses);
+
+        try {
+            const token = await getToken();
+            const { data } = await axios.get(backendUrl + '/api/user/enrolled-courses', { headers: { Authorization: `Bearer ${token}` } });
+
+            if (data.success) {
+                setEnrolledCourses(data.enrolledCourses.reverse());
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
     }
 
     useEffect(() => {
@@ -71,19 +126,20 @@ export const AppContextProvider = (props) => {
         fetchUserEnrolledCourses();
     }, []);
 
-    const logToken = async () => {
-        console.log(await getToken());
-    }
+    // const logToken = async () => {
+    //     console.log(await getToken());
+    // }
 
     useEffect(() => {
         if (user) {
-            logToken();
+            // logToken();
+            fetchUserData();
         }
-    })
+    }, [user]);
 
     const value = {
         // Add shared state or functions here
-        currency, allCourses, navigate, calculateRating, isEducator, calculateChapterTime, calculateCourseDuration, calculateNoOfLectures, enrolledCourses, fetchUserEnrolledCourses
+        currency, allCourses, navigate, calculateRating, isEducator, calculateChapterTime, calculateCourseDuration, calculateNoOfLectures, enrolledCourses, fetchUserEnrolledCourses, backendUrl, userData, setUserData, getToken, fetchAllCourses
     }
 
     return (
