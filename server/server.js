978c79c23b9b14999098d274c64421082b2422d3
nikhillad123 <nikhil.lad24india@gmail.com>
2025-08-15 08:53:08 +0530

    Creating webhooks and User and doing git to get CLERK WEBHOOK SECRET

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js';
-// import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js';
+import { clerkWebhooks } from './controllers/webhooks.js';
 // import educatorRouter from './routes/educatorRoutes.js';
 // import { clerkMiddleware } from '@clerk/express';
 // import connectCloudinay from './configs/cloudinary.js';
@@ -25,13 +25,11 @@ app.use(cors());
 
 // Routes
 app.get('/', (req,res)=>{res.send("Edemy API is working fine!")})
-// app.post('/clerk', express.json(), clerkWebhooks)
+app.post('/clerk', express.json(), clerkWebhooks)
 // app.use('/api/educator', express.json(), educatorRouter);
 // app.use('/api/course', express.json(), courseRouter);
 // app.use('/api/user', express.json(), userRouter);
 // app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
-
-
 
 // port
 const PORT = process.env.PORT || 3000;
